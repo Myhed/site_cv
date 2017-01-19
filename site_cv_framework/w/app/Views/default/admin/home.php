d0f840082e5d110b91d11920ab3c5f7c4f88c929
Myhed <capuchondu92@hotmail.fr>
2017-01-19 21:19:45 +0100

    Modification de homeAdmin (Afficahge des titre des noms des champs

--- site_cv_framework/w/app/Views/default/admin/home.php
+++ site_cv_framework/w/app/Views/default/admin/home.php
@@ -1,12 +1,19 @@
+<?php
+$t_experience = array();
 
+$t_experience = array_slice($columns[2],20,25);
+$t_competence = array_slice($columns[2],17,+3);
+				
+?>
 
 <?php $this->layout('layout.admin',array('title' => 'Home',
 										'LayoutLateralBar' => 'Mes infos')); ?>
 
 <?php $this->start('main_content');?>
-<?php var_dump($datas[2][0]); ?>
 
 <?php $this->stop('main_content');?>
+
+
 
 		
 <?php $this->start('sidebar');?>
@@ -19,7 +26,7 @@
 				<?php endforeach;?>
 						
 				<?php foreach($datas[0] as $key => $data):?>
-						<?php var_dump();?>
+						
 				<?php endforeach;?>
 		</table>
 	</div>
@@ -27,13 +34,17 @@
 <?php $this->stop('sidebar');?>
 <?php var_dump();?>
 	<?php $this->start('sidebar-2');?>
-		<table class="table table-2">	
-					<?php foreach($columns[2] as $column):?>
-					
-					<!-- ?php echo'<th class="text-center">'.$column['name'].'</th>'?> -->
+		<table class="table table-2">
 
-					<?php var_dump($column);?>
+					
+					<?php foreach($t_experience as $column):?>
+					
+					<?php echo'<th class="text-center">'.$column['name'].'</th>'; ?>
+
+					
 				<?php endforeach;?>
+
+			
 		</table>
 	<?php $this->stop('sidebar-2');?>
 
@@ -43,4 +54,18 @@
 
 	<?php $this->start('sidebar-3');?>
 
+
+		<table class="table table-3">
+
+					
+					<?php foreach($t_competence as $column):?>
+					
+					<?php echo'<th class="text-center">'.$column['name'].'</th>'; ?>
+
+					
+				<?php endforeach;?>
+
+				
+		
+				</table>
 	<?php $this->stop('sidebar-3');?>
