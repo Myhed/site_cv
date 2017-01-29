80fd185c91bc1bb8356b1c9d9a2b72371cd1c30b
Myhed <capuchondu92@hotmail.fr>
2017-01-29 21:05:31 +0100

    finission du design css

--- site_cv/index.php
+++ site_cv/index.php
@@ -6,20 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
    
     <title>Starter Template for Bootstrap</title>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/app.css">
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
+   
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -28,22 +23,25 @@
   </head>
 
   <body>
-
+  
     <nav class="navbar navbar-default">
+     <a class="navbar-brand" href="#"><p>Myhed.</p>Ben-Rhouma</p></a>
       <div class="container">
         <div class="navbar-header">
+
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img class="img" src="assets/img/logo.png" alt="" height="50"></a>
+          
+         
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li ><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </div><!--/.nav-collapse -->
@@ -63,17 +61,92 @@
 
         <div class="section-a">
           <div class="container">
-                
+                <div class="row">
+                  <div class="col-md-4">
+                      <i class="fa fa-html5"></i>
+                      <h3>Apprentissage de l'html 5</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime hic excepturi, at quidem non beatae. Quos sit necessitatibus, ducimus, saepe sapiente sint esse, quo, odit dolor assumenda in praesentium corporis.</p>
+                  </div>
+                  <div class="col-md-4">
+                      <i class="fa fa-gear"></i> 
+                      <h3>Site fait avec SASS et GULP</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime hic excepturi, at quidem non beatae. Quos sit necessitatibus, ducimus, saepe sapiente sint esse, quo, odit dolor assumenda in praesentium corporis.</p>
+                  </div>
+                  <div class="col-md-4">
+                      <i class="fa fa-lightbulb-o"></i> 
+                      <h3>Customisable</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime hic excepturi, at quidem non beatae. Quos sit necessitatibus, ducimus, saepe sapiente sint esse, quo, odit dolor assumenda in praesentium corporis.</p>
+                  </div>
+                </div>
           </div>
         </div>
 
+        <div class="section-b">
+          <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                      
+                            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                                   <div class="panel panel-default">
+                                         <div class="panel-heading" role="tab" id="headingOne">
+                                               <h4 class="panel-title">
+                                                      <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                      Collapsible Group Item #1
+                                                       </a>
+                                              </h4>
+                                        </div>
+                                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                              <div class="panel-body">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                             </div>
+                                      </div>
+                              <!-- FIN de la div panel-collapse -->
+                                </div>
+                         <!-- FIN de la div panel panel-default -->
+                        </div>
+                    <!-- FIN de la div panel-group -->
+                      
+              </div>
+              <!-- FIN de la div col-md-6 -->
+                  <div class="col-md-6">
+
+                         <div class="progress skill-bar ">
+                               <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                               <span class="skill">HTML <i class="val">100%</i></span>
+                              </div>
+                        </div>
+                
+                     
+                          </div>
+                     <!-- FIN de la div col-md-6 -->
+                    </div>
+                <!-- FIN de la div row -->
+              </div>
+                <!-- FIN de la div container -->
+        </div>
+        <!-- FIN de la div section-b -->
+
+        <div class="section-c"> 
+            <div class="container"> 
+                <div class="row"> 
+                    <div class="col-md-8">
+                        <p>hEY</p>
+                    </div>
+                </div>
+           </div>
+
+           <footer> 
+                <div class="container"> 
+                  <p>Copyright &copy; 2017 Myhed theme</p>
+                </div>
+           </footer>
+
     <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+      
+    
+    <script src="assets/bower_components/jquery/dist/jquery.js"></script>
+    <script src="assets/js/Utilitaire.js"></script>
+    <script src="assets/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
   </body>
 </html>
